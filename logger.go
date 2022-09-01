4fd48d5f30d3f65a9c84427e03b4127f5d73c7db
Marcelo Schmitz <marcelo.schmitz@itschmitz.com>
2022-09-01 18:34:17 -0300

    feat: log lib

---
+++ logger.go
@@ -0,0 +1,18 @@
+package logging
+
+import (
+	"os"
+
+	"github.com/rs/zerolog"
+)
+
+type Logger struct {
+	Standard *zerolog.Logger
+}
+
+func NewLogger() *Logger {
+	logLevel := zerolog.InfoLevel
+	zerolog.SetGlobalLevel(logLevel)
+	logger := zerolog.New(os.Stderr).With().Timestamp().Logger()
+	return &Logger{Standard: &logger}
+}
